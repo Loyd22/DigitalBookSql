4c70848c02c9e8c252366bcaa4c7bfbf756268ad
Viray, John Loyd <115643874+Loyd22@users.noreply.github.com>
2024-07-19 19:58:54 +0800

    Add files via upload

---
+++ digiBookBusiness/Business.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using digiBookModel;
+using digiBookDataLayer;
+
+namespace digiBookBusiness
+{
+    public class Business
+    {
+        public List<bookss> GetAllBooks()
+        {
+            return Data.GetBooks();
+        }
+
+        public bool AddBook(string title, string author, string summary)
+        {
+            return Data.AddBook(new bookss { title = title, author = author, summary = summary });
+        }
+
+        public bool UpdateBook(string title, string author, string summary)
+        {
+            return Data.UpdateBook(new bookss { title = title, author = author, summary = summary });
+        }
+    }
+}
