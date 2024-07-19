9a52ee9ce099d10bce4fc4688b4bb24f38139156
Viray, John Loyd <115643874+Loyd22@users.noreply.github.com>
2024-07-19 19:51:22 +0800

    Add files via upload

---
+++ digiBookDataLayer/Data.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using digiBookModel;
+
+namespace digiBookDataLayer
+{
+    public static class Data
+    {
+        public static List<bookss> GetBooks()
+        {
+            sqldbdata dataAccess = new sqldbdata();
+            return dataAccess.GetBooks();
+        }
+
+        public static bool AddBook(bookss book)
+        {
+            sqldbdata dataAccess = new sqldbdata();
+            return dataAccess.AddBook(book);
+        }
+
+        public static bool UpdateBook(bookss book)
+        {
+            sqldbdata dataAccess = new sqldbdata();
+            return dataAccess.UpdateBook(book);
+        }
+    }
+}
