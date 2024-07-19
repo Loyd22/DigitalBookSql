78d5e51c9f57971e13874039a9cb649d4eabdd7f
Viray, John Loyd <115643874+Loyd22@users.noreply.github.com>
2024-07-19 19:48:50 +0800

    Add files via upload

---
+++ bookdemo2/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using digiBookBusiness;
+using digiBookModel;
+
+namespace bookdemos2.API.Controllers
+{
+    [ApiController]
+    [Route("api/books")]
+    public class BookController : ControllerBase
+    {
+        private readonly Business _business;
+
+        public BookController()
+        {
+            _business = new Business();
+        }
+
+        [HttpGet]
+        public IEnumerable<bookss> GetBooks()
+        {
+            var books = _business.GetAllBooks();
+
+            List<bookss> bookList = new List<bookss>();
+
+            foreach (var book in books)
+            {
+                bookList.Add(new bookss
+                {
+                    title = book.title,
+                    author = book.author,
+                    summary = book.summary
+                });
+            }
+
+            return bookList;
+        }
+
+        [HttpPost]
+        public JsonResult AddBook([FromBody] bookss request)
+        {
+            var result = _business.AddBook(request.title, request.author, request.summary);
+            return new JsonResult(result);
+        }
+
+        [HttpPatch]
+        public JsonResult UpdateBook([FromBody] bookss request)
+        {
+            var result = _business.UpdateBook(request.title, request.author, request.summary);
+            return new JsonResult(result);
+        }
+    }
+}
---
+++ bookdemo2/bookss.cs
@@ -0,0 +1,11 @@
+﻿namespace bookdemo2
+{
+    public class bookss
+
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+        public int summary { get; set; }
+
+    }
+}
